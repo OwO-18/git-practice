11895eb10b3e479ea88c20a75d03ea5a05d9b2af
OwO-18 <80306000+OwO-18@users.noreply.github.com>
2024-09-23 02:34:23 +0800

    add fib_iter

--- week-02/fib.js
+++ week-02/fib.js
@@ -8,3 +8,20 @@ function fib_rec(n) {
 for(let i = 0; i< 11; i++){
 	console.log(fib_rec(i)); //check output
 }
+
+function fib_iter(n) {
+  if (n <= 1) {
+    return n;      	
+  }
+  let a0 = 0, a1 = 1, ans = 1;
+  for(let i = 2; i <= n; i++){
+  ans = a0 + a1;
+  a0 = a1;
+  a1 = ans;
+  }
+  return ans;
+}
+
+for(let i = 0; i< 11; i++){
+	console.log(fib_iter(i)); //check output
+}
