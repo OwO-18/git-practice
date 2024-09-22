ff7c8418c5ec7aeebb42c883445ba30021e92a16
OwO-18 <80306000+OwO-18@users.noreply.github.com>
2024-09-23 02:25:09 +0800

    Update fib.js

--- week-02/fib.js
+++ week-02/fib.js
@@ -1,8 +1,10 @@
 function fib_rec(n) {
   if (n <= 1) {
-    return n;
+    return n;      	
   }
   return fib_rec(n - 1) + fib_rec(n - 2);
 }
 
-console.log(fib_rec(10)); 
+for(let i = 0; i< 11; i++){
+	console.log(fib_rec(i)); //check output
+}
