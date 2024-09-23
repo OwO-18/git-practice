28bd1308b517c33897936ce81be28339c4db43f5
OwO-18 <80306000+OwO-18@users.noreply.github.com>
2024-09-23 17:07:03 +0800

    Create stack.js

---
+++ week-02/stack.js
@@ -0,0 +1,52 @@
+// stack.js
+// 完成以下 TODO 的部分，並且以 Module 的方式匯出 (ESM)
+
+export default class Stack {
+  // TODO: # 有特別的意思嗎？請以註解回覆。
+  //在變數前面加上#的話會使得這個變數變成「私有變數」，代表這個變數只會在這個class內被取用。這樣做的好處是main code不會不小心access到class裡面的變數，避免不必要的麻煩。
+  #items;
+
+  constructor() {
+    this.#items = [];
+  }
+
+  // 在 stack 頂部加入元素i
+  push(element) {
+	  this.#items.push(element);
+  }
+
+  // 移除並回傳 stack 頂部的元素
+  pop() {
+		return this.#items.pop();
+  }
+
+  // 回傳 stack 頂部的元素，但不移除它
+  peek() {
+    temp = this.#items.pop();
+    push(element);
+    return temp;
+  }
+
+  // 檢查 stack 是否為空
+  isEmpty() {
+    if(this.#items.length == 0){
+      return true;
+  }
+    else
+      return false;
+
+  // 回傳 stack 中元素的個數
+  size() {
+    return this.#items.length + 1;
+  }
+
+  // 清空 stack 
+  clear() {
+    // TODO
+  }
+
+  // 印出 stack 內容
+  print() {
+    // TODO
+  }
+}
