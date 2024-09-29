7b0f4051ee7c8bc10ab0012c0a26271a74523b30
OwO-18 <80306000+OwO-18@users.noreply.github.com>
2024-09-30 02:10:43 +0800

    write callback.js

--- week-03/callback.js
+++ week-03/callback.js
@@ -10,8 +10,31 @@ function doJob(job, time, cb) {
 let now = new Date();
 console.log(`開始工作 at ${now.toISOString()}`);
 // write your code here
-// 以下是使用範例
+
 doJob('刷牙', 1000, function (data) {
-  // 表示 doJob 做完了
   console.log(data);
+  
+  doJob('吃早餐', 3000, function (data) {
+    console.log(data);
+    
+    doJob('寫功課', 1000, function (data) {
+      console.log(data);
+      
+      doJob('吃午餐', 2000, function (data) {
+        console.log(data);
+      });
+    });
+  });
 });
+
+/*
+Output:
+
+/Users/ben/.nvm/versions/node/v20.17.0/bin/node ./test.js
+開始工作 at 2024-09-29T18:08:45.489Z
+完成工作 刷牙 at 2024-09-29T18:08:46.509Z
+完成工作 吃早餐 at 2024-09-29T18:08:49.511Z
+完成工作 寫功課 at 2024-09-29T18:08:50.513Z
+完成工作 吃午餐 at 2024-09-29T18:08:52.514Z
+
+*/
